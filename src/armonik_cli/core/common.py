f8b992c5a91ef73724391f143cf4c5b04d5c554a
qdelamea <qdelamea@aneo.fr>
2025-01-17 10:47:25 +0100

    feat: add global option support

---
+++ src/armonik_cli/core/common.py
@@ -0,0 +1,85 @@
+import rich_click as click
+
+from typing import Callable, Any
+from typing_extensions import TypeAlias
+
+from armonik_cli.core.options import GlobalOption
+
+
+ClickOption: TypeAlias = Callable[[Callable[..., Any]], Callable[..., Any]]
+
+
+endpoint_option = click.option(
+    "-e",
+    "--endpoint",
+    type=str,
+    required=True,
+    help="Endpoint of the cluster to connect to.",
+    metavar="ENDPOINT",
+    cls=GlobalOption,
+)
+
+
+output_option = click.option(
+    "-o",
+    "--output",
+    type=click.Choice(["yaml", "json", "table"], case_sensitive=False),
+    default="json",
+    show_default=True,
+    help="Commands output format.",
+    metavar="FORMAT",
+    cls=GlobalOption,
+)
+
+
+debug_option = click.option(
+    "--debug",
+    is_flag=True,
+    default=False,
+    help="Print debug logs and internal errors.",
+    cls=GlobalOption,
+)
+
+
+def apply_click_params(
+    command: Callable[..., Any], *click_options: ClickOption
+) -> Callable[..., Any]:
+    """
+    Applies multiple Click options to a command.
+
+    Args:
+        command: The Click command function to decorate.
+        *click_options: The Click options to apply.
+
+    Returns:
+        The decorated command function.
+    """
+    for click_option in click_options:
+        command = click_option(command)
+    return command
+
+
+def global_cluster_config_options(command: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Adds global cluster configuration options to a Click command.
+
+    Args:
+        command: The Click command function to decorate.
+
+    Returns:
+        The decorated command function.
+    """
+    return apply_click_params(command, endpoint_option)
+
+
+def global_common_options(command: Callable[..., Any]) -> Callable[..., Any]:
+    """
+    Adds global common options such as output format and debug mode to a Click command.
+
+    Args:
+        command: The Click command function to decorate.
+
+    Returns:
+        The decorated command function.
+    """
+    return apply_click_params(command, output_option, debug_option)
