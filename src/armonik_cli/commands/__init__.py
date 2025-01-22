d4eab0a949a54ccacc087a6e8c82f389654a742a
sleep <abde.arrahman.dk@gmail.com>
2025-01-22 17:12:23 +0100

    Added ArmoniK cluster commands

--- src/armonik_cli/commands/__init__.py
+++ src/armonik_cli/commands/__init__.py
@@ -2,6 +2,7 @@ from .sessions import sessions
 from .tasks import tasks
 from .partitions import partitions
 from .results import results
+from .cluster import cluster
 
 
-__all__ = ["sessions", "tasks", "partitions", "results"]
+__all__ = ["sessions", "tasks", "partitions", "results", "cluster"]
