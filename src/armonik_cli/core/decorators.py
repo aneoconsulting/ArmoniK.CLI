cc6efe7fcb71dc5719a738593a1d2ef994c6b4ed
sleep <abde.arrahman.dk@gmail.com>
2025-01-22 16:01:55 +0100

    Rewrote the error handler for better error message clarity

--- src/armonik_cli/core/decorators.py
+++ src/armonik_cli/core/decorators.py
@@ -1,12 +1,14 @@
 import grpc
-import rich_click as click
 
 from functools import wraps, partial
 from typing import Callable, Optional, Any
 
 from armonik_cli.core.console import console
 from armonik_cli.core.common import global_cluster_config_options, global_common_options
-from armonik_cli.exceptions import NotFoundError, InternalError, InternalArmoniKError
+from armonik_cli.exceptions import (
+    InternalCliError,
+    InternalArmoniKError,
+)
 
 
 def error_handler(func: Optional[Callable[..., Any]] = None) -> Callable[..., Any]:
@@ -24,28 +26,36 @@ def error_handler(func: Optional[Callable[..., Any]] = None) -> Callable[..., An
         return partial(error_handler)
 
     @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
+    def wrapper(*args, **kwargs):
+        debug_mode = kwargs.get("debug", False)
         try:
             return func(*args, **kwargs)
-        except click.ClickException:
-            raise
         except grpc.RpcError as err:
             status_code = err.code()
-            error_details = f"{err.details()}."
+            error_details = f"{err.details()} (gRPC Code: {status_code.name})."
 
-            if status_code == grpc.StatusCode.NOT_FOUND:
-                raise NotFoundError(error_details)
-            elif status_code == grpc.StatusCode.INTERNAL:
-                raise InternalArmoniKError(f"An internal exception has occurred:\n{error_details}")
-            elif status_code == grpc.StatusCode.UNKNOWN:
-                raise InternalArmoniKError(f"An unknown exception has occurred:\n{error_details}")
-            else:
-                raise InternalError("An internal fatal error occurred.")
-        except Exception:
-            if kwargs.get("debug", False):
+            if debug_mode:
                 console.print_exception()
+
+            if status_code == grpc.StatusCode.INVALID_ARGUMENT:
+                raise InternalCliError(error_details) from err
+            elif status_code == grpc.StatusCode.NOT_FOUND:
+                raise InternalArmoniKError(error_details) from err
+            elif status_code == grpc.StatusCode.ALREADY_EXISTS:
+                raise InternalArmoniKError(error_details) from err
+            elif status_code == grpc.StatusCode.DEADLINE_EXCEEDED:
+                raise InternalArmoniKError(error_details) from err
+            elif status_code == grpc.StatusCode.INTERNAL:
+                raise InternalArmoniKError(error_details) from err
+            elif status_code == grpc.StatusCode.UNKNOWN:
+                raise InternalArmoniKError(error_details) from err
             else:
-                raise InternalError("An internal fatal error occurred.")
+                raise InternalArmoniKError(error_details) from err
+
+        except Exception as e:
+            if debug_mode:
+                console.print_exception()
+            raise InternalCliError(f"CLI errored with exception:\n{e}") from e
 
     return wrapper
 
